79a358748e8f43cfbb3a2b186f7b57200d71030e
hafizrahmadhani <mhafiz.rahmadhani@gmail.com>
2021-10-01 11:45:12 +0700

    Initial commit

---
+++ app/src/main/java/com/hafizrahmadhani/myapplication/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.hafizrahmadhani.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hafizrahmadhani.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.foundation.lazy.items
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp{
+                MyScreenContent()
+            }
+//            MyApplicationTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(color = MaterialTheme.colors.background) {
+//                    Greeting("Android")
+//                }
+//            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    MyApplicationTheme {
+        Surface(color = Color.Yellow) {
+            content()
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = Color.Yellow) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
+}
+
+@Composable
+fun MyScreenContent(names : List<String> = List(1000) {"Hello Android #$it"}){
+    val counterState = remember { mutableStateOf(0)}
+
+    Column(modifier = Modifier.fillMaxHeight()) {
+        Column(modifier = Modifier.weight(1f)) {
+            for (name in names) {
+                Greeting(name = name)
+                Divider(color = Color.Black)
+            }
+        }
+        Counter(
+            count = counterState.value,
+            updateCount = {newCount ->
+                counterState.value = newCount
+            }
+        )
+    }
+}
+
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit){
+    Button(
+        onClick = {updateCount(count+1)},
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = if(count > 5) Color.Green else Color.White
+        )
+    ) {
+        Text("I've been clicked $count times")
+    }
+}
+
+@Composable
+fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(items = names) { name ->
+            Greeting(name = name)
+            Divider(color = Color.Black)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+//    MyApplicationTheme {
+//        Greeting("Android")
+//    }
+    MyApp{
+        MyScreenContent()
+    }
+}
